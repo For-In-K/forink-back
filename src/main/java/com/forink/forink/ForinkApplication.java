86da49475ef603e573f56b9f11036a520b646bc7
JJimini <min102602@pusan.ac.kr>
2025-07-07 15:53:06 +0900

    init : 프로젝트 초기 세팅

---
+++ src/main/java/com/forink/forink/ForinkApplication.java
@@ -0,0 +1,13 @@
+package com.forink.forink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forink/forink/ForinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forink.forink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
