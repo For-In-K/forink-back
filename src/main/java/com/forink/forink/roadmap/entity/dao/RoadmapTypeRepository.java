04649e7f853a7db14a8b52a28b574e9be1438c20
JJimini <min102602@pusan.ac.kr>
2025-07-20 00:29:41 +0900

    chore: 오타 수정

--- src/main/java/com/forink/forink/roadmap/entity/dao/RoadmapTypeRepository.java
+++ src/main/java/com/forink/forink/roadmap/entity/dao/RoadmapTypeRepository.java
@@ -1,6 +1,7 @@
 package com.forink.forink.roadmap.entity.dao;
 
+import com.forink.forink.roadmap.entity.RoadmapType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoadmapTypeRepository extends JpaRepository<RoadmapTypeRepository, Long> {
+public interface RoadmapTypeRepository extends JpaRepository<RoadmapType, Long> {
 }
