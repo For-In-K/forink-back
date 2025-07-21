a39c4eef744ceac3144e67ffe54d002ed19d6da8
HSHwan <tnghks2866@gmail.com>
2025-07-21 18:37:58 +0900

    refactor: 구글 OAuth 설정에서 @ConfigurationProperties를 @Value로 변경

--- src/main/java/com/forink/forink/auth/utils/GoogleClient.java
+++ src/main/java/com/forink/forink/auth/utils/GoogleClient.java
@@ -1,27 +1,42 @@
 package com.forink.forink.auth.utils;
 
-import com.forink.forink.auth.utils.property.GoogleOAuthProperty;
-import com.forink.forink.auth.utils.property.GoogleProviderProperty;
 import com.google.api.client.auth.oauth2.AuthorizationCodeRequestUrl;
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class GoogleClient {
 
-    private final GoogleOAuthProperty googleOAuthProperty;
-    private final GoogleProviderProperty googleProviderProperty;
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+    private String clientSecret;
+
+    @Value("${spring.security.oauth2.client.registration.google.redirect-uri}")
+    private String redirectUri;
+
+    @Value("${spring.security.oauth2.client.registration.google.scope}")
+    private List<String> scopes;
+
+    @Value("${spring.security.oauth2.client.provider.google.authorization-uri}")
+    private String authorizationUri;
+
+    @Value("${spring.security.oauth2.client.provider.google.token-uri}")
+    private String tokenUri;
+
+    @Value("${spring.security.oauth2.client.provider.google.user-info-uri}")
+    private String userInfoUri;
 
     public String createGoogleAuthorizationUrl() {
         String state = UUID.randomUUID().toString();
-        return new AuthorizationCodeRequestUrl(
-                googleProviderProperty.authorizationUri(),
-                googleOAuthProperty.clientId()
-        )
-                .setRedirectUri(googleOAuthProperty.redirectUri())
-                .setScopes(googleOAuthProperty.scope())
+        return new AuthorizationCodeRequestUrl(authorizationUri, clientId)
+                .setRedirectUri(redirectUri)
+                .setScopes(scopes)
                 .setState(state)
                 .build();
     }
