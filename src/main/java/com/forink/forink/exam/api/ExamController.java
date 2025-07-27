ed0cb952f2783fa58c0c59a2a4fa68beef25be2a
JJimini <min102602@pusan.ac.kr>
2025-07-27 23:08:24 +0900

    feat: 진단 검사 생성 API 개발

--- src/main/java/com/forink/forink/exam/api/ExamController.java
+++ src/main/java/com/forink/forink/exam/api/ExamController.java
@@ -1,4 +1,31 @@
 package com.forink.forink.exam.api;
 
+import static org.springframework.http.HttpStatus.CREATED;
+
+import com.forink.forink.exam.application.ExamService;
+import java.net.URI;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/exams")
 public class ExamController {
+
+    private final ExamService examService;
+
+    // todo: member 정보 받아오는 로직 필요
+    @PostMapping
+    public ResponseEntity<Void> createExam() {
+        examService.createExam();
+
+        return ResponseEntity.status(CREATED)
+                .location(URI.create("/exams"))
+                .build();
+    }
 }
--- src/main/java/com/forink/forink/exam/application/ExamService.java
+++ src/main/java/com/forink/forink/exam/application/ExamService.java
@@ -1,4 +1,39 @@
 package com.forink.forink.exam.application;
 
+import static com.forink.forink.member.entity.MemberRoleType.ROLE_회원;
+
+import com.forink.forink.exam.entity.Exam;
+import com.forink.forink.exam.entity.dao.ExamRepository;
+import com.forink.forink.member.entity.Member;
+import com.forink.forink.member.entity.dao.MemberRepository;
+import java.util.ArrayList;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class ExamService {
+
+    private final ExamRepository examRepository;
+    private final MemberRepository memberRepository;
+
+    public void createExam() {
+        // todo : 로그인 기능 개발 완료 시 수정 필요
+        final Member tempMember = createFakeMember();
+        examRepository.save(Exam.builder()
+                .member(tempMember)
+                .build());
+    }
+
+    private Member createFakeMember() {
+        return memberRepository.save(Member.builder()
+                .email("temp@pusan.ac.kr")
+                .google_id("tempId")
+                .name("tempName")
+                .mode(ROLE_회원)
+                .roadmaps(new ArrayList<>())
+                .build());
+    }
 }
