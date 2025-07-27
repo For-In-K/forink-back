ec8d814ca038b316614b8adf5df39456e0e51d60
JJimini <min102602@pusan.ac.kr>
2025-07-27 21:05:37 +0900

    fix: 진단검사 결과 관련 컬럼 자료형을 enum에서 Integer로 수정

--- src/main/java/com/forink/forink/exam/entity/ExamStep.java
+++ src/main/java/com/forink/forink/exam/entity/ExamStep.java
@@ -3,8 +3,6 @@ package com.forink.forink.exam.entity;
 import com.forink.global.base.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -32,14 +30,13 @@ public class ExamStep extends BaseEntity {
     @Column(nullable = false)
     private Integer stepNumber;
 
-    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private ExamAnswerType mode;
+    private Integer answer;
 
     @Builder
-    private ExamStep(final Exam exam, final Integer stepNumber, final ExamAnswerType mode) {
+    private ExamStep(final Exam exam, final Integer stepNumber, final Integer answer) {
         this.exam = exam;
         this.stepNumber = stepNumber;
-        this.mode = mode;
+        this.answer = answer;
     }
 }
