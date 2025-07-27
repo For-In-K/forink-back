cb3f95f6ea4dab80db2ac4015827cb6a3073384c
JJimini <min102602@pusan.ac.kr>
2025-07-27 23:24:55 +0900

    chore: 회원의 진단검사에 unique 제약 추가

--- src/main/java/com/forink/forink/exam/entity/Exam.java
+++ src/main/java/com/forink/forink/exam/entity/Exam.java
@@ -3,7 +3,6 @@ package com.forink.forink.exam.entity;
 import static com.forink.forink.exam.entity.StatusType.IN_PROGRESS;
 
 import com.forink.forink.member.entity.Member;
-import com.forink.forink.member.entity.MemberRoleType;
 import com.forink.global.base.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -14,7 +13,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -31,7 +29,7 @@ public class Exam extends BaseEntity {
     private Long id;
 
     @OneToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "member_id", nullable = false)
+    @JoinColumn(name = "member_id", nullable = false, unique = true)
     private Member member;
 
     @Enumerated(EnumType.STRING)
