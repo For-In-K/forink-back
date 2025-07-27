462c647d16f56a2051e14c330e2d12e73f62744c
HSHwan <tnghks2866@gmail.com>
2025-07-27 21:35:55 +0900

    refactor: 구글 OAuth2 scopes 상수를 프로퍼티 주입 방식(@Value)으로 변경 및 불필요 설정 제거

--- src/main/java/com/forink/forink/auth/util/GoogleClient.java
+++ src/main/java/com/forink/forink/auth/util/GoogleClient.java
@@ -14,29 +14,21 @@ public class GoogleClient {
     @Value("${spring.security.oauth2.client.registration.google.client-id}")
     private String clientId;
 
-    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
-    private String clientSecret;
-
     @Value("${spring.security.oauth2.client.registration.google.redirect-uri}")
     private String redirectUri;
 
+    @Value("${spring.security.oauth2.client.registration.google.scope}")
+    private List<String> scopes;
+
     @Value("${spring.security.oauth2.client.provider.google.authorization-uri}")
     private String authorizationUri;
-
-    @Value("${spring.security.oauth2.client.provider.google.token-uri}")
-    private String tokenUri;
-
-    @Value("${spring.security.oauth2.client.provider.google.user-info-uri}")
-    private String userInfoUri;
-
-    private static final List<String> DEFAULT_SCOPES = List.of("openid", "profile", "email");
 
     public String createGoogleAuthorizationUrl() {
         String state = UUID.randomUUID().toString();
         return new AuthorizationCodeRequestUrl(authorizationUri, clientId)
                 .setRedirectUri(redirectUri)
                 .setState(state)
-                .setScopes(DEFAULT_SCOPES)
+                .setScopes(scopes)
                 .build();
     }
 
