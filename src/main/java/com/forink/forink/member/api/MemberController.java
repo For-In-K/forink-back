bf09a224a08a91673021f7a615771a3c671d5a1e
HSHwan <tnghks2866@gmail.com>
2025-07-30 03:08:01 +0900

    refactor: Auth 패키지 정리

--- src/main/java/com/forink/forink/member/api/MemberController.java
+++ src/main/java/com/forink/forink/member/api/MemberController.java
@@ -1,4 +1,39 @@
 package com.forink.forink.member.api;
 
+import com.forink.forink.member.application.MemberService;
+import java.net.URI;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/oauth/google")
+    public ResponseEntity<Void> redirectToGoogleOAuth() {
+        String redirectURL = memberService.getGoogleLoginRedirectURL();
+
+        URI redirectUri = UriComponentsBuilder.fromUriString(redirectURL)
+                .encode()
+                .build()
+                .toUri();
+
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .location(redirectUri)
+                .build();
+    }
+
+    @GetMapping("/oauth/google/callback")
+    public ResponseEntity<?> handleGoogleOAuthCallback(@RequestParam String code) {
+        return ResponseEntity.ok()
+                .build();
+    }
+
 }
--- src/main/java/com/forink/forink/member/application/MemberService.java
+++ src/main/java/com/forink/forink/member/application/MemberService.java
@@ -1,4 +1,20 @@
 package com.forink.forink.member.application;
 
+import com.forink.forink.global.security.util.GoogleClient;
+import com.forink.forink.member.entity.dao.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    private final GoogleClient googleClient;
+
+    public String getGoogleLoginRedirectURL() {
+        return googleClient.createGoogleAuthorizationUrl();
+    }
+
 }
