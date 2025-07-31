5354177682dd77a856d2db2df968f1bf69359973
HSHwan <tnghks2866@gmail.com>
2025-07-31 12:03:45 +0900

    fix: Redirect URI 생성 방식 변경

--- src/main/java/com/forink/forink/member/api/MemberController.java
+++ src/main/java/com/forink/forink/member/api/MemberController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,14 +19,8 @@ public class MemberController {
     @GetMapping("/oauth/google")
     public ResponseEntity<Void> redirectToGoogleOAuth() {
         String redirectURL = memberService.getGoogleLoginRedirectURL();
-
-        URI redirectUri = UriComponentsBuilder.fromUriString(redirectURL)
-                .encode()
-                .build()
-                .toUri();
-
         return ResponseEntity.status(HttpStatus.FOUND)
-                .location(redirectUri)
+                .location(URI.create(redirectURL))
                 .build();
     }
 
