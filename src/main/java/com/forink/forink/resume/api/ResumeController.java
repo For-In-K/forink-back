6a270923fba903d96113131ee5fe7549f078f908
HSHwan <tnghks2866@gmail.com>
2025-07-31 14:05:30 +0900

    feat: 가이드 이력서 생성 기능 구현

--- src/main/java/com/forink/forink/resume/api/ResumeController.java
+++ src/main/java/com/forink/forink/resume/api/ResumeController.java
@@ -1,4 +1,27 @@
 package com.forink.forink.resume.api;
 
+import com.forink.forink.global.security.annotation.LoginMember;
+import com.forink.forink.member.entity.Member;
+import com.forink.forink.resume.application.ResumeService;
+import java.net.URI;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/guides/resume")
 public class ResumeController {
+
+    private final ResumeService resumeService;
+
+    @PostMapping
+    public ResponseEntity<Void> createResume(@LoginMember final Member member) {
+        resumeService.createResume(member);
+        return ResponseEntity.created(URI.create("/guides/resume/1"))
+                .build();
+    }
+
 }
--- src/main/java/com/forink/forink/resume/application/ResumeService.java
+++ src/main/java/com/forink/forink/resume/application/ResumeService.java
@@ -1,4 +1,23 @@
 package com.forink.forink.resume.application;
 
+import com.forink.forink.member.entity.Member;
+import com.forink.forink.resume.entity.Resume;
+import com.forink.forink.resume.entity.dao.ResumeRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ResumeService {
+
+    private final ResumeRepository resumeRepository;
+
+    @Transactional
+    public void createResume(Member member) {
+        resumeRepository.save(Resume.builder()
+                .member(member)
+                .build());
+    }
+
 }
