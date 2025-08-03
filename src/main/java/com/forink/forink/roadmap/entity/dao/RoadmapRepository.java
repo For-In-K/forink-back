0a0b0103d797d09dd3f167683ee5ef2a307b8c24
JJimini <min102602@pusan.ac.kr>
2025-08-03 14:39:50 +0900

    chore: 사용하지 않는 코드 삭제

--- src/main/java/com/forink/forink/roadmap/entity/dao/RoadmapRepository.java
+++ src/main/java/com/forink/forink/roadmap/entity/dao/RoadmapRepository.java
@@ -4,12 +4,9 @@ import com.forink.forink.member.entity.Member;
 import com.forink.forink.roadmap.entity.Roadmap;
 import com.forink.forink.roadmap.entity.RoadmapType;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoadmapRepository extends JpaRepository<Roadmap, Long> {
-
-    Optional<Roadmap> findByMember(final Member member);
 
     List<Roadmap> findAllByMember(final Member member);
 
