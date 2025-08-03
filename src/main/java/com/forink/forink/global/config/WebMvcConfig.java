9e79aa6b30ecc843a2d3def848730022e35e1c67
HSHwan <tnghks2866@gmail.com>
2025-08-03 16:58:37 +0900

    feat: 커스텀 ArgumentResolver 등록을 위한 WebMvcConfigurer 추가

---
+++ src/main/java/com/forink/forink/global/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.forink.forink.global.config;
+
+import com.forink.forink.global.security.annotation.MemberArgumentResolver;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final MemberArgumentResolver memberArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(memberArgumentResolver);
+    }
+}
