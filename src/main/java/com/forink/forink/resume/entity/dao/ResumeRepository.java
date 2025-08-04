aeace8030c7ce7f8f3b42aa7df9ddfbc2e1e9a38
HSHwan <tnghks2866@gmail.com>
2025-08-04 11:07:44 +0900

    feat: 가이드 전체 조회 기능 구현

--- src/main/java/com/forink/forink/resume/entity/dao/ResumeRepository.java
+++ src/main/java/com/forink/forink/resume/entity/dao/ResumeRepository.java
@@ -1,10 +1,16 @@
 package com.forink.forink.resume.entity.dao;
 
+import com.forink.forink.member.entity.MemberRoleType;
 import com.forink.forink.resume.entity.Resume;
+import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ResumeRepository extends JpaRepository<Resume, Long> {
 
     Optional<Resume> findByMember_Id(Long memberId);
+
+    @EntityGraph(attributePaths = {"member"})
+    List<Resume> findAllByMember_MemberRoleType(MemberRoleType memberMemberRoleType);
 }
