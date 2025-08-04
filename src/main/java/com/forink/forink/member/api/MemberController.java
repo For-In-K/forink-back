80067e591e0b8603ca9336e07a0930333ed26e44
HSHwan <tnghks2866@gmail.com>
2025-08-04 09:18:54 +0900

    fix: NotBlank 및 Validated annotation 추가

--- src/main/java/com/forink/forink/member/api/MemberController.java
+++ src/main/java/com/forink/forink/member/api/MemberController.java
@@ -2,14 +2,17 @@ package com.forink.forink.member.api;
 
 import com.forink.forink.member.application.MemberService;
 import com.forink.forink.member.application.dto.response.OAuthLoginResponse;
+import jakarta.validation.constraints.NotBlank;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
@@ -25,7 +28,7 @@ public class MemberController {
     }
 
     @GetMapping("/oauth/google/callback")
-    public ResponseEntity<OAuthLoginResponse> handleGoogleOAuthCallback(@RequestParam("code") String code) {
+    public ResponseEntity<OAuthLoginResponse> handleGoogleOAuthCallback(@NotBlank @RequestParam("code") String code) {
         return ResponseEntity.ok(memberService.processGoogleOAuthCallback(code));
     }
 
