4ed0834fb2057fa469d5eada05449cead7a94610
JJimini <min102602@pusan.ac.kr>
2025-08-10 22:48:18 +0900

    fix: 진단검사 권한 처리 추가

--- src/main/java/com/forink/forink/exam/api/ExamController.java
+++ src/main/java/com/forink/forink/exam/api/ExamController.java
@@ -12,6 +12,7 @@ import java.net.URI;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/exams")
+@Secured("ROLE_회원")
 public class ExamController {
 
     private final ExamService examService;
